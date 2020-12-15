ed80b1e6a5e9fefe39a3bedb78a92445cc37e40c
Preston <pres.10.nap@gmail.com>
2020-12-14 20:13:50 -0500

    updated with old code

--- src/components/pages/search/SearchForm.js
+++ src/components/pages/search/SearchForm.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Pagination } from 'antd';
+import {
+  Field,
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Radio,
+  Card,
+  Pagination,
+} from 'antd';
+import SkeletonButton from 'antd/lib/skeleton/Button';
+// import { SearchPagination } from './SearchPagination.js';
 import { getGroomerData } from '../../../api/index';
-import { Link } from 'react-router-dom';
-import GroomerDispaly from '../ProfileDisplay/GroomerDisplay';
-import GroomerDisplay from '../ProfileDisplay/GroomerDisplay';
+import Geocode from 'react-geocode';
+
+Geocode.setApiKey('AIzaSyDvbprCrQ-zJnjwdimEwzJHO5LULTR_vtg');
+Geocode.setLanguage('en');
+Geocode.setRegion('us');
 
 const cardDescription = {
   margin: '1px',
@@ -23,19 +36,28 @@ const tailLayout = {
   },
 };
 
+const layoutForm = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayoutForm = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 const { Meta } = Card;
 
 const SearchForm = () => {
   const [name, setName] = useState('');
   const [zipcode, setZipcode] = useState('');
   const [groomers, setGroomers] = useState([]);
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(12);
-
-  const handleChange = value => {
-    setMinValue((value - 1) * 12);
-    setMaxValue(value * 12);
-  };
+  const [form] = Form.useForm();
 
   const onFinish = values => {
     console.log('Success: groomers displayed', values);
@@ -54,17 +76,160 @@ const SearchForm = () => {
   useEffect(() => {
     getGroomerData().then(response => {
       setGroomers(response);
+      // Geocode.fromAddress(groomers.zip).then(
+      //   response => {
+      //     const { lat, lng } = response.results[0].geometry.location;
+      //     // filterLocation(lat, lng, latInput, lngInput);
+      //     console.log(lat, lng);
+      //   },
+      //   error => {
+      //     console.error(error);
+      //   }
+      // );
     });
   }, []);
+
+  //for the form, use the below to add a toggle option
+  // const onOptionChange = (value) => {
+  //   switch (value) {
+  //     case 'example':
+  //       form.setFieldsValue({
+  //         note: 'Leave a message here',
+  //       });
+  //       return;
+  //   }
+  // };
+
+  // function filterLocation(lat, lng, latInput, lngInput){
+  //   var latOut = latInput-lat;
+  //   var lngOut = lngInput-lng;
+  //   distance = (lngOut.abs)*(latOut.abs)/2;
+  //   return distance;
+  // }
+  const filterDist = (lng, lat) => {
+    groomers.map(groomer => {
+      const groomLng = parseInt(groomer.longitude, 10);
+      const groomLat = parseInt(groomer.latitude, 10);
+      console.log(groomLat, groomLng);
+      const distance = (lng - groomLng + (lat - groomLat)) / 2;
+      const sorted = [...groomers].sort(
+        (a, b) => b[Math.abs(distance)] - a[Math.abs(distance)]
+      );
+      console.log(distance);
+      setGroomers(sorted);
+    });
+  };
+
+  const onFormFinish = values => {
+    setZipcode(values.zip);
+
+    Geocode.fromAddress(values.zip).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        filterDist(lng, lat);
+        console.log(lat, lng, groomers);
+      },
+      error => {
+        console.error(error);
+      }
+    );
+
+    console.log(values.zip);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      zip: '44101',
+    });
+  };
+  //for the form ^^^^
 
   console.log(groomers);
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', margin: '10px' }}>
-      {groomers &&
-        groomers.length > 0 &&
-        groomers.slice(minValue, maxValue).map(groomer => (
-          <Link to={`/groomers/${groomer.id}`}>
+    <div>
+      <div>
+        <Form
+          {...layoutForm}
+          form={form}
+          name="control-hooks"
+          onFinish={onFormFinish}
+        >
+          <Form.Item
+            name="zip"
+            label="Zip Code"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          {/* <Form.Item
+        name="Example"
+        label="Example"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Select
+          placeholder="Use this to add options later"
+          onChange={onOptionChange}
+          allowClear
+        >
+          <Option value="example">example</Option>
+        </Select>
+      </Form.Item> */}
+
+          {/* ^^^^^ this is to set an option, maybe for pet breeds or options for services? */}
+
+          <Form.Item
+            noStyle
+            shouldUpdate={(prevValues, currentValues) =>
+              prevValues.Example !== currentValues.Example
+            }
+          >
+            {({ getFieldValue }) => {
+              return getFieldValue('Example') === 'other' ? (
+                <Form.Item
+                  name="customizeExample"
+                  label="Customize Example"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+              ) : null;
+            }}
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button htmlType="button" onClick={onReset}>
+              Reset
+            </Button>
+            <Button type="link" htmlType="button" onClick={onFill}>
+              Fill form
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', margin: '10px' }}>
+        {groomers.map(groomer => {
+          return (
             <Card
               hoverable
               style={{
@@ -74,8 +239,11 @@ const SearchForm = () => {
               cover={<img alt="example" src={groomer.photo_url} />}
             >
               <Meta title={groomer.name + ' ' + groomer.lastname}></Meta>
-              <div style={{ marginBottom: '1px' }}>
-                {GroomerDisplay(groomer)}# pass
+              <div
+                style={{
+                  marginBottom: '1px',
+                }}
+              >
                 <p style={cardDescription}>
                   Vet Visit Rate: ${groomer.vet_visit_rate}
                 </p>
@@ -90,14 +258,9 @@ const SearchForm = () => {
                 <p style={cardDescription}>{groomer.country}</p>
               </div>
             </Card>
-          </Link>
-        ))}
-      <Pagination
-        defaultCurrent={1}
-        defaultPageSize={12}
-        onChange={handleChange}
-        total={100}
-      />
+          );
+        })}
+      </div>
     </div>
   );
 };
