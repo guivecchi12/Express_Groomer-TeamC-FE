1a79d4a2100a916a00286f74d3b7e1a83544f803
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-12-16 21:38:38 -0500

    fixed more pathing issues in SearchForm.js

--- src/components/search/SearchForm.js
+++ src/components/search/SearchForm.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Pagination } from 'antd';
-import { getGroomerData } from '../../../api/index';
+import { getGroomerData } from '../../api/index';
 import { Link } from 'react-router-dom';
-import GroomerDispaly from '../ProfileDisplay/GroomerDisplay';
-import GroomerDisplay from '../../groomers/GroomerDisplay';
+// import GroomerDispaly from '../ProfileDisplay/GroomerDisplay';
+import GroomerDisplay from '../groomers/GroomerDisplay';
 
 const cardDescription = {
   margin: '1px',
