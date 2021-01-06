0f413ac40da5ae6d1c698aedf620112b1d1029aa
austinkelsay <austinkelsay@yahoo.com>
2021-01-05 19:09:33 -0600

    Basic component setup, I'm laying out what information needs to be on the petcard and just rendering it as aplaceholder right now

--- src/components/customers/pets/petDisplay.js
+++ src/components/customers/pets/petDisplay.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Button } from 'antd';
 
-const petDisplay = () => {
-  return <div>Test</div>;
+const AddPet = {
+  margin: '10px',
 };
 
-export default petDisplay;
+const PetDisplay = () => {
+  return (
+    <div className="pet-display">
+      <Button type="primary" htmlType="submit" style={AddPet}>
+        Add a new pet
+      </Button>
+      <div>Name</div>
+      <div>*image of pet*</div>
+      <div>Animal</div>
+      <div>Breed</div>
+      <div>Age</div>
+      <div>Weight</div>
+      <div>Personality / quirks</div>
+      <div>Vaccinations</div>
+    </div>
+  );
+};
+
+export default PetDisplay;
