c56218247f0f22e04f8749cb974f116cc0964666
Taylor Palmer <taylorjadepalmer@gmail.com>
2021-01-07 17:12:57 -0800

    centered registration page

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  padding: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  padding: 3%;
+`;
 
 const Registration = props => {
   return (
-    <div>
+    <StyledDiv>
       <h2>Are you a...</h2>
-      <Link
-        to={{
-          pathname: '/register/groomers',
-          state: {
-            email: props.email,
-          },
-        }}
-      >
-        Groomer
-      </Link>
-      <Link
-        to={{
-          pathname: '/register/customers',
-          state: {
-            email: props.email,
-          },
-        }}
-      >
-        Customer
-      </Link>
-    </div>
+      <div>
+        <StyledLink
+          to={{
+            pathname: '/register/groomers',
+            state: {
+              email: props.email,
+            },
+          }}
+        >
+          Groomer
+        </StyledLink>
+        <StyledLink
+          to={{
+            pathname: '/register/customers',
+            state: {
+              email: props.email,
+            },
+          }}
+        >
+          Customer
+        </StyledLink>
+      </div>
+    </StyledDiv>
   );
 };
 
