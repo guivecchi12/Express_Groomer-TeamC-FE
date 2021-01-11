e54d4196e65ac4f82da70665ad63a8cbcf0cdafb
Taylor Palmer <taylorjadepalmer@gmail.com>
2021-01-10 18:48:04 -0800

    styled registration.js

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -3,25 +3,42 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-  padding: 5%;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
+  background-color: navy;
+`;
+
+const StyledDiv2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
 `;
 
 const StyledLink = styled(Link)`
   display: flex;
   flex-direction: row;
-  padding: 3%;
+  padding: 15%;
+  font-size: 3em;
+  color: white;
+`;
+
+const StyledH2 = styled.h2`
+  padding: 10%;
+  font-size: 2.5em;
+  color: white;
 `;
 
 const Registration = props => {
   return (
     <StyledDiv>
-      <h2>Are you a...</h2>
-      <div>
+      <StyledH2>Are you a...</StyledH2>
+      <StyledDiv2>
         <StyledLink
           to={{
             pathname: '/register/groomers',
@@ -42,7 +59,7 @@ const Registration = props => {
         >
           Customer
         </StyledLink>
-      </div>
+      </StyledDiv2>
     </StyledDiv>
   );
 };
