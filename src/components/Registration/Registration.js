0ad1ca893a347706bc4fe98a702af73e35319dbe
Taylor Palmer <taylorjadepalmer@gmail.com>
2021-01-14 18:07:13 -0800

    Imgs on groom/cust btns in reg.js. Added more styling

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import petOwnerPic from './images/female-dog-groomer-brushing.jpg';
-import groomerPic from './images/petowner.jpg';
+import groomerPic from './images/female-dog-groomer-brushing.jpg';
+import petOwnerPic from './images/petowner.jpg';
 
 const StyledDiv = styled.div`
   width: 100%;
@@ -15,52 +15,60 @@ const StyledDiv = styled.div`
   background-color: #1d2d44;
 `;
 
-const ImgContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-`;
 const StyledDiv2 = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   width: 100%;
+  height: 100vh;
 `;
 
-const StyledLink = styled(Link)`
+const GroomerLink = styled(Link)`
   display: flex;
   flex-direction: row;
-  padding: 25% 15%;
+  justify-content: flex-end;
+  padding: 3% 3%;
+  width: 48%;
+  height: 100%;
   font-size: 3em;
-  color: #f0f9f9;
+  color: #9ad4d6;
+  background-image: url(${groomerPic});
+  background-size: 140%;
+  background-repeat: no-repeat;
+  border-radius: 5px;
+`;
+
+const CustomerLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  padding: 3% 3%;
+  width: 48%;
+  font-size: 3em;
+  color: #c5e8e8;
+  background-image: url(${petOwnerPic});
+  background-size: 157%;
+  background-repeat: no-repeat;
+  border-radius: 5px;
 `;
 
 const StyledH2 = styled.h2`
-  padding-top: 10%;
+  margin: 0% 0% 3% 0%;
+  padding: 4% 0% 1% 0%;
   font-size: 2.5em;
   color: #f0f9f9;
-`;
-
-const StyledGroomerImg = styled.div`
-  float: left;
-  background-image: url(${groomerPic});
-`;
-
-const StyledCustImg = styled.div`
-  float: right;
-  background-image: url(${petOwnerPic});
+  width: 100%;
+  height: 4%;
+  background-color: #3e5c76;
+  text-align: center;
 `;
 
 const Registration = props => {
   return (
     <StyledDiv>
-      {/* <ImgContainer> */}
-      <StyledGroomerImg />
-      {/* <StyledCustImg /> */}
-      {/* </ImgContainer> */}
       <StyledH2>Are you a...</StyledH2>
       <StyledDiv2>
-        <StyledLink
+        <GroomerLink
           to={{
             pathname: '/register/groomers',
             state: {
@@ -69,8 +77,8 @@ const Registration = props => {
           }}
         >
           Groomer
-        </StyledLink>
-        <StyledLink
+        </GroomerLink>
+        <CustomerLink
           to={{
             pathname: '/register/customers',
             state: {
@@ -79,7 +87,7 @@ const Registration = props => {
           }}
         >
           Customer
-        </StyledLink>
+        </CustomerLink>
       </StyledDiv2>
     </StyledDiv>
   );
