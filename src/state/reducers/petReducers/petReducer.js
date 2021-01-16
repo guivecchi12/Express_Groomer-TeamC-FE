e569f9310ef7bdf74c9eeeb31de095b128b7a5c2
austinkelsay <austinkelsay@yahoo.com>
2021-01-15 18:19:48 -0600

    Filled in all of the cases for petReducer

---
+++ src/state/reducers/petReducers/petReducer.js
@@ -0,0 +1,100 @@
+import {
+  GET_PET_INFO_START,
+  GET_PET_INFO_SUCCESS,
+  GET_PET_INFO_FAILURE,
+  REGISTER_PET_INFO_START,
+  REGISTER_PET_INFO_SUCCESS,
+  REGISTER_PET_INFO_FAILURE,
+  UPDATE_PET_START,
+  UPDATE_PET_SUCCESS,
+  UPDATE_PET_FAILURE,
+  DELETE_PET_START,
+  DELETE_PET_SUCCESS,
+  DELETE_PET_FAILURE,
+} from '../types';
+
+const initialState = {
+  PET: {},
+  isFetching: false,
+  error: '',
+  status: '',
+};
+
+export const PETReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PET_INFO_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case GET_PET_INFO_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        PET: action.payload,
+      };
+    case GET_PET_INFO_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload,
+      };
+    case REGISTER_PET_INFO_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case REGISTER_PET_INFO_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        PET: action.payload,
+      };
+    case REGISTER_PET_INFO_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload,
+      };
+    case UPDATE_PET_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case UPDATE_PET_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        PET: action.payload,
+        status: 'success',
+      };
+    case UPDATE_PET_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload,
+        status: 'failure',
+      };
+    case DELETE_PET_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case DELETE_PET_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        PET: action.payload,
+        status: 'success',
+      };
+    case DELETE_PET_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload,
+        status: 'failure',
+      };
+    default:
+      return state;
+  }
+};
